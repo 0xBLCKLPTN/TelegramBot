56d08d7222973d25922a839e4ba4b509a2f2192a
Даниил Ермолаев <daniilermolaev@MacBook-Pro-Daniil.local>
2022-11-22 18:42:15 +0500

    Шаблон классов

--- src/utils/dataspace.py
+++ src/utils/dataspace.py
@@ -1,1 +1,17 @@
+from db import db
+from models.models import Admins, Users
+
+session = db.create_database()
+
+class ManageAdmins:
+    def remove_admin(self):
+        pass
+
+    def add_admin(self):
+        pass
+
+class Registration:
+    def add_user(self, user: Users):
+        pass
+    
 
