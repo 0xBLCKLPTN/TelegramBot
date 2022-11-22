20b45c5bf06a4415f443a18c48331e497bdc939e
Noimpass <andraver731@gmail.com>
2022-11-22 20:12:46 +0500

    asd

--- src/start.py
+++ src/start.py
@@ -1,6 +1,18 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
 
+from keyboards.start_menu import *
+
+bot = Bot(token="5843120216:AAEKuj9NkCahZM5Mu1-lQDoBfQ7lGXkEeJk")
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=["start"])
+async def process_start_command(message: types.Message):
+    await message.reply("hui", reply_markup=start_kb)
 
 
 
 if __name__ == '__main__':
+    executor.start_polling(dp)
     pass
