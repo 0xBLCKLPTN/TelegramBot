0ab746400a7df53c5f2269ce8dd9a7d88f8d3127
Даниил Ермолаев <daniilermolaev@MacBook-Pro-Daniil.local>
2022-11-22 19:07:18 +0500

    Coverage tests

--- src/tests.py
+++ src/tests.py
@@ -1,4 +1,14 @@
-from db import db
-from models.models import Admins, Users
+#from db import db
+#from models.models import Admins, Users
+import unittest
+from utils import validator
+#session = db.create_database()
 
-session = db.create_database()
+class TestValidator(unittest.TestCase):
+    def test_name(self):
+        name = 'Аргустян Валерий Ебанович'
+        self.assertEqual(validator.check_name(name), True)
+    
+    def test_email(self):
+        email = 'blcklptn@gmail.com'
+        self.assertEqual(validator.check_email(email), True)
--- src/utils/dataspace.py
+++ src/utils/dataspace.py
@@ -1,5 +1,6 @@
 from db import db
 from models.models import Admins, Users
+from sqlalchemy.exc import NoResultFound
 
 session = db.create_database()
 
@@ -10,8 +11,18 @@ class ManageAdmins:
     def add_admin(self):
         pass
 
-class Registration:
+class ManageUsers:
     def add_user(self, user: Users):
         pass
-    
+
+    def check_user(self, user_id: int) -> bool:
+        try:
+            record = session.query(Users).filter(Users.user_id == user_id).one()
+            return True
+        except NoResultFound:
+            return False
+
+
+k = ManageUsers().check_user('123')
+print(k)
 
