3fcc816b8213cd7b95c34a03fa2a9a191f9e026b
Даниил Ермолаев <daniilermolaev@MacBook-Pro-Daniil.local>
2022-11-22 18:51:54 +0500

    check revenue

--- src/utils/validator.py
+++ src/utils/validator.py
@@ -1,5 +1,6 @@
 from email_validator import validate_email, EmailNotValidError
 
+
 def check_email(email: str) -> bool:
     try:
         validation = validate_email(email)
@@ -7,8 +8,17 @@ def check_email(email: str) -> bool:
     except EmailNotValidError as ex:
         return False
 
+
 def check_name(name: str) -> bool:
     if len(name.split(' ')) == 3:
         return True
     else:
         return False
+
+
+def check_revenue(revenue) -> bool:
+    if isinstance(revenue, int):
+        return True
+    else:
+        return False
+
