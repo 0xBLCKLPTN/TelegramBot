78b4fd246fc13c5b1dcbec3c3944c7f934549cfe
Даниил Ермолаев <daniilermolaev@MacBook-Pro-Daniil.local>
2022-11-23 10:53:53 +0500

    Delete library call comment from dispatcher

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -1,7 +1,6 @@
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
-#from core.config import settings
 
 bot = Bot(token="5843120216:AAEKuj9NkCahZM5Mu1-lQDoBfQ7lGXkEeJk")
 dp = Dispatcher(bot)
