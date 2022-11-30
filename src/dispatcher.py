0bee0a447f67260fb55a9438ba7f69a6a218ada3
Daniil Ermolaev <blcklptn@gmail.com>
2022-11-30 16:46:12 +0500

    test action

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -5,7 +5,6 @@ from core.config import settings
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
 
-
 storage = MemoryStorage()
 bot = Bot(token=settings.BOT_TOKEN)
 dp = Dispatcher(bot, storage=storage)
